c54222ac699727ac2fbbd0ec912a15d284026b7c
Joe Seidel <seidel.jp@gmail.com>
2016-09-15 16:52:48 -0500

    worker to upfront10

--- worker.py
+++ worker.py
@@ -45,7 +45,7 @@ def job():
                 print response
                 mturker, created = Mturker.objects.get_or_create(user_id=user.id)
                 #tc = TreatmentCell.objects.filter(batch=_BATCH).filter(finished=0).order_by('?')[0]
-                tc = TreatmentCell.objects.filter(batch=_BATCH).filter(treatment="endog")[0]
+                tc = TreatmentCell.objects.filter(batch=_BATCH).filter(treatment="upfront50")[0]
                 mturker.assign_treatmentcell(tc.id, response['workerId'], response['assignmentId'])
                 feedback = "You can use this code: {} to login at https://tranquil-meadow-42703.herokuapp.com".format(mturker.user.username)
                 mturk.notify_workers([response['workerId']], "Assess Code Verified", feedback)
