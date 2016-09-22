063a21ff9b1f5602f3dd0c48c9cfcb8022533311
Joe Seidel <seidel.jp@gmail.com>
2016-09-22 10:43:25 -0500

    endog

--- worker.py
+++ worker.py
@@ -45,7 +45,7 @@ def job():
                 print response
                 mturker, created = Mturker.objects.get_or_create(user_id=user.id)
                 #tc = TreatmentCell.objects.filter(batch=_BATCH).filter(finished=0).order_by('?')[0]
-                tc = TreatmentCell.objects.filter(batch=_BATCH).filter(treatment="upfront50")[0]
+                tc = TreatmentCell.objects.filter(batch=_BATCH).filter(treatment="endog")[0]
                 mturker.assign_treatmentcell(tc.id, response['workerId'], response['assignmentId'])
                 feedback = "Your response to our Mturk HIT was just auto-approved and your $0.10 reward was paid on your Amazon Payment account.  Please use this code: {} to login at https://tranquil-meadow-42703.herokuapp.com to receive information about our short task and earn EXTRA payment as a bonus.".format(mturker.user.username)
                 mturk.notify_workers([response['workerId']], "Assess Code Verified", feedback)
